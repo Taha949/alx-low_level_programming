c3712830563a0baf3ff34df083f38a25dcfbda46
taha949 <maatataha@gmail.com>
2023-03-19 09:57:53 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-/** main - Entry point
+/**
+ * main - Entry point
  *
  * Return: always 0
- */
+*/
 
-int main (void)
+int main(void)
 {
 char ch;
 
-for (ch= 'a' ; ch <= 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 putchar(ch);
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 putchar(ch);

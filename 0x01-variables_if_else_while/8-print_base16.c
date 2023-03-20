fb197014fc8d664b302e0a140bf54bc81eecb090
taha949 <maatataha@gmail.com>
2023-03-20 07:08:08 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        int num;
+	char ch;
+
+        for (num = 0; num < 10; num++)
+        putchar((num % 10) + '0');
+	for (ch = 'a'; ch < 'g'; ch++)
+	putchar(ch);
+
+        putchar('\n');
+
+        return (0);
+}

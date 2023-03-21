d0388aca20218831f41bd11b5c7361d22de46290
taha949 <maatataha@gmail.com>
2023-03-21 16:17:03 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: the character 
+ * Return: 1 0 
+ */
+
+int _isalpha(int c)
+{
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}

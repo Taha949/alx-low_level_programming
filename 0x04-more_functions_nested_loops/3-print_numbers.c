3391e94c9afa8cdb0c6494b8390bb9e9d5635cb2
taha949 <maatataha@gmail.com>
2023-03-24 18:29:21 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
+*print_numbers - entry point
 *Return: void
 */
 
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+	_putchar((c % 10) + '0');
 	}
 	_putchar('\n');
 }

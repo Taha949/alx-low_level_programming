46b6c6c9f2b23564ea3632bb23f7462956b60be9
taha949 <maatataha@gmail.com>
2023-03-30 18:24:44 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strncpy - entry point
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: a pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
+	return (dest);
+
+}
+

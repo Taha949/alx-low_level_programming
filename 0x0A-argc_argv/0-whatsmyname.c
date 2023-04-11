03b1beb67cb2999a14c8505e05d6aec6c1437967
taha949 <maatataha@gmail.com>
2023-04-10 18:54:56 -0700

    0. It ain t what they call you, it s what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: a char
+ * @argv: an int
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
+

4a80e67c4ec26929a28b87351d6d6167b1d769a1
taha949 <maatataha@gmail.com>
2023-04-17 16:13:06 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - entry.
+ * @name: first member
+ * @age: second member
+ * @owner: thid member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * dog_t - Entry point
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif

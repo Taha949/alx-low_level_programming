75fe619157af48b99929c63ada52db88af547049
taha949 <maatataha@gmail.com>
2023-04-27 18:24:35 -0700

    0. Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,1 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - entry point
+ * @str: a string
+ * @len: an int
+ * @next: an iint
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
+

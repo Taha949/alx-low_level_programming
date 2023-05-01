ff69afb094ebb31ecd53e24b15eb0be9062a0036
taha949 <maatataha@gmail.com>
2023-05-01 14:08:03 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	if (h == NULL) {
-		return (0);
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num ++;
 	}
-
-	size_t num = 1;
-	printf("%d\n", h->n);
-
-	num += print_listint(h->next);
 
 	return (num);
 }

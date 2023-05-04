97e00b1d88ee0b5e844733b1283337d33f1f3343
taha949 <maatataha@gmail.com>
2023-05-04 15:54:09 -0700

    0. 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - entry point
+ * @b: string
+ *
+ * Return: int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int result = 0;
+    int len = 0;
+
+    if (b == NULL)
+        return 0;
+
+    while (b[len] != '\0')
+        len++;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (b[i] == '1')
+            result += (1u << (len - i - 1));
+        else if (b[i] != '0')
+            return 0;
+    }
+
+    return result;
+}
+
